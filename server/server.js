81ce666a14536c5ee3e8d015f44ebd9b08a4b5f1
Timo Tuominen <timo.tuominen@gmail.com>
2012-03-05 23:50:09 +0000

    Prepare express to serve coffee script files as js.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 var coffee = require("coffee-script"),
-    express = require("express");
+    express = require("express"),
+    fs = require("fs");
 
 var webroot = './public';
 
 exports.open = function (port) {
 
     var app = express.createServer();
-    app.use(express.static(webroot));
+
+    app.get("*", function (req, res) {
+        var path = "./public/" + req.params[0];
+        if (path.substr(path.length-1) === "/") {
+            path += "index.html";
+        }
+        res.sendfile(path);
+    });
+
     app.listen(port);
 
     console.log("express running at http://localhost:%d", port);
