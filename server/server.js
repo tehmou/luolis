24a4f6afd5911529e27f1ab12398d8e9fe6ba152
Timo Tuominen <timo.tuominen@gmail.com>
2012-03-05 23:35:50 +0000

    Switched to using express.js for the server.

--- server/server.js
+++ server/server.js
@@ -1,34 +1,13 @@
-var static = require('node-static'),
-    coffee = require('coffee-script'),
-    http = require('http');
+var coffee = require("coffee-script"),
+    express = require("express");
 
 var webroot = './public';
 
 exports.open = function (port) {
-    var file = new(static.Server)(webroot, {
-        cache: 600,
-        headers: { 'X-Powered-By': 'node-static' }
-    });
 
-    http.createServer(function(req, res) {
+    var app = express.createServer();
+    app.use(express.static(webroot));
+    app.listen(port);
 
-        req.addListener('end', function() {
-            file.serve(req, res, function(err, result) {
-                if (err) {
-                    console.error('Error serving %s - %s', req.url, err.message);
-                    if (err.status === 404 || err.status === 500) {
-                        file.serveFile(err.status+'.html', err.status, {}, req, res);
-                    } else {
-                        res.writeHead(err.status, err.headers);
-                        res.end();
-                    }
-                } else {
-                    console.log(res);
-                    console.log('%s - %s', req.url, res.message);
-                }
-            });
-        });
-    }).listen(port);
-
-    console.log('node-static running at http://localhost:%d', port);
+    console.log("express running at http://localhost:%d", port);
 };
