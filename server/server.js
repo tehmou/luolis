2cabe9ecbe84901f14daffc9b39a9b4fbf89bf0e
Timo Tuominen <timo.tuominen@gmail.com>
2012-03-07 00:15:01 +0000

    Add coffee script support to server.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var coffee = require("coffee-script"),
     express = require("express"),
-    fs = require("fs");
+    fs = require("fs"),
+    path = require("path");
 
 var webroot = './public';
 
@@ -9,11 +10,31 @@ exports.open = function (port) {
     var app = express.createServer();
 
     app.get("*", function (req, res) {
-        var path = "./public/" + req.params[0];
-        if (path.substr(path.length-1) === "/") {
-            path += "index.html";
+        var reqPath = "./public" + req.params[0];
+        if (reqPath.substr(reqPath.length-1) === "/") {
+            reqPath += "index.html";
         }
-        res.sendfile(path);
+
+        console.log("Requested file " + reqPath);
+
+        if (reqPath.match(/\.js$/)) {
+            var coffeePath = reqPath.replace(/\.js$/, ".coffee");
+            if (path.existsSync(coffeePath)) {
+                console.log("Compile coffee for " + coffeePath);
+                try {
+                    var coffeeJS = coffee.compile(""+fs.readFileSync(coffeePath));
+                    res.send(coffeeJS);
+                } catch (err) {
+                    res.send(err.message);
+                }
+            } else {
+                console.log("Serving JS file " + reqPath);
+                res.sendfile(reqPath);
+            }
+        } else {
+            console.log("Serving file " + reqPath);
+            res.sendfile(reqPath);
+        }
     });
 
     app.listen(port);
